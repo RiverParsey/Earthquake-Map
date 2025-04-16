ab31d81bf8c35b2a731eb224cf3c85110fcadc7a
ricsi <rihardptrk18@gmail.com>
2025-04-16 12:27:58 +0200

    Initial commit

---
+++ gulp/config/plugins.js
@@ -0,0 +1,12 @@
+import replace from "gulp-replace";
+import notify from "gulp-notify";
+import browsersync from 'browser-sync';
+
+export const plugins = {
+
+  replace: replace,
+  notify: notify,
+  browsersync: browsersync,
+
+}
+
